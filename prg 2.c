39daedd4b36712712498a5ce30f2d81acd1a1e5a
vikkram-devops-engineer <vickytamilarasi23052001@gmail.com>
2025-11-18 01:53:16 +0530

    upload a local file to my first demo

---
+++ prg 2.c
@@ -0,0 +1,22 @@
+ #include <stdio.h>
+ int main()
+ {
+     float  var1 = 3+9;
+     short int var2 = 34/2;
+ printf("%lf\n", var1);
+ printf("%d\n", var2 );
+ printf("%d\n", sizeof(int));
+ printf("%d\n", sizeof( signed short int));
+ printf("%d\n", sizeof( signed long int));
+ printf("%d\n", sizeof(double));
+ printf("%d\n", sizeof(float));
+ printf("%d\n", sizeof(long double));
+ printf("%d\n", sizeof(signed char));
+ printf("%d\n", sizeof(unsigned int));
+ printf("%d\n", sizeof( unsigned short int));
+ printf("%d\n", sizeof( unsigned long int));
+ printf("%d\n", sizeof(unsigned char));
+ printf("%d", printf("%10s\n", "hello"));
+
+ return 0;
+ }
